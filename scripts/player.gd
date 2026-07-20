cacdf80ee715ae02d052e69db4001b0431c0cdc0
Andrew Gilliland <andrewpgilliland@gmail.com>
2026-07-20 00:02:54 -0500

    Play jump animation while airborne

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,14 +20,19 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		if _animated_sprite != null and _animated_sprite.sprite_frames != null:
-			if _animated_sprite.sprite_frames.has_animation(&"walk_1"):
-				_animated_sprite.play(&"walk_1")
-			_animated_sprite.flip_h = direction < 0.0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if _animated_sprite != null and _animated_sprite.sprite_frames != null:
-			if _animated_sprite.sprite_frames.has_animation(&"idle"):
-				_animated_sprite.play(&"idle")
+
+	if _animated_sprite != null and _animated_sprite.sprite_frames != null:
+		if direction:
+			_animated_sprite.flip_h = direction < 0.0
+
+		if not is_on_floor():
+			if _animated_sprite.sprite_frames.has_animation(&"jump"):
+				_animated_sprite.play(&"jump")
+		elif direction and _animated_sprite.sprite_frames.has_animation(&"walk_1"):
+			_animated_sprite.play(&"walk_1")
+		elif _animated_sprite.sprite_frames.has_animation(&"idle"):
+			_animated_sprite.play(&"idle")
 
 	move_and_slide()
